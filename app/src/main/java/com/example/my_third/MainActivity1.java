ba42347976b9cd641e4967b18d7f67d976d06e43
13930167513 <839193001@qq.com>
2019-12-18 17:39:20 +0800

    1

--- app/src/main/java/com/example/my_third/MainActivity1.java
+++ app/src/main/java/com/example/my_third/MainActivity1.java
@@ -3,7 +3,7 @@ package com.example.my_third;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
